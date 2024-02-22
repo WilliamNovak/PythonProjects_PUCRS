0c79c33decae265326e6f1ee72bca2420606611c
William Novak <novakwilliamr@gmail.com>
2024-02-22 20:27:37 -0300

    ano bissexto dias mes

--- Aula4/dias_no_mes.py
+++ Aula4/dias_no_mes.py
@@ -1,6 +1,7 @@
 # Receba o mês e informe quantos dias tem
 try:
     m = int(input('Informe o mês do ano (1-12): '))
+    a = int(input('Informe o ano: '))
 except:
     print('Valor inválido!')
 
@@ -8,7 +9,10 @@ if m < 1 or m > 12:
     print('Mês deve estar entre 1 e 12.')
 else:
     if m == 2:
-        d = 28
+        if (a%400 == 0) or (a%4 == 0 and a%100 != 0):
+            d = 29
+        else:
+            d = 28
     elif m == 4 or m == 6 or m == 9 or m == 11:
         d = 30
     else:
