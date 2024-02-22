13349da615e8f4508a8ff59e942fa6bd02b03314
William Novak <novakwilliamr@gmail.com>
2024-02-21 21:01:48 -0300

    calculo media ponderada

--- Aula3/exercicio7.py
+++ Aula3/exercicio7.py
@@ -7,8 +7,10 @@ try:
 except:
     print('Nota inválida!')
 
-if n1 >= n2 and n1 >= n3: m = (n1 * 5 + n2 * 2.5 + n3 * 2.5)/10
-elif n2 >= n1 and n2 >= n3: m = (n2 * 5 + n1 * 2.5 + n3 * 2.5)/10
-else: m = (n3 * 5 + n1 * 2.5 + n2 * 2.5)/10
-
-print('Média ponderada:',m)
+if n1 < 0 or n1 > 10 or n2 < 0 or n2 > 10 or n3 < 0 or n3 > 10:
+    print('Notas devem estar no intervalo de 0 a 10.')
+else:
+    if n1 >= n2 and n1 >= n3: m = (n1 * 5 + n2 * 2.5 + n3 * 2.5)/10
+    elif n2 >= n1 and n2 >= n3: m = (n2 * 5 + n1 * 2.5 + n3 * 2.5)/10
+    else: m = (n3 * 5 + n1 * 2.5 + n2 * 2.5)/10
+    print('Média ponderada:',m)
