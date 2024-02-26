4b9b7aac0ec8fce84beba377f80ccf5f200b0578
William Novak <novakwilliamr@gmail.com>
2024-02-26 19:46:35 -0300

    tabuada

---
+++ Aula6/tabuada.py
@@ -0,0 +1,8 @@
+# Tabuada de 1 a 10
+numero = 1
+while numero <= 10:
+    multi = 1
+    while multi <= 10:
+        print(f'{numero} X {multi} = {numero*multi}')
+        multi += 1
+    numero += 1
