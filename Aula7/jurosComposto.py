8c4ec7d5ede7daef6a020c99aedfeeb2c9900635
William Novak <novakwilliamr@gmail.com>
2024-03-01 09:19:56 -0300

    juros composto e escopo

---
+++ Aula7/jurosComposto.py
@@ -0,0 +1,13 @@
+def calcula(base, duracao):
+    global taxa # acessa a variavel global no escopo local para poder alterar valor (nao recomendado)
+    taxa = taxa + 0.1
+    final = base * (1+taxa) ** duracao
+    return final
+
+b = float(input('Valor inicial: '))
+taxa = float(input('Taxa de juros mensal: ')) #variavel global
+d = int(input('Duração (meses): '))
+
+res = calcula(b,d)
+print(f'Valor do final do período: {res:.2f}')
+print(f'Valor da taxa de juros: {taxa}')
