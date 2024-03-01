c2a2651fc14cd2f8d27570886dfce121f96db36b
William Novak <novakwilliamr@gmail.com>
2024-03-01 14:51:48 -0300

    coeficiente binomial

--- Aula7/coeficienteBinomial.py
+++ Aula7/coeficienteBinomial.py
@@ -8,5 +8,16 @@ def fatorial(num):
 def binomial(n,k):
     return fatorial(n) // (fatorial(k) * fatorial(n-k))
 
+def binomial2(n,k): # forma mais eficiente
+    prod = 1
+    total = k
+    if n-k < k:
+        total = n-k
+    for i in range(1,total+1):
+        prod = prod * ((n+1-i)/i)
+    return int(prod)
+
 print(binomial(4,2))
 print(binomial(20,10))
+print(binomial2(4,2))
+print(binomial2(20,10))
