ce7f0e776d0209a3dd8a5b364991482885843ab6
William Novak <novakwilliamr@gmail.com>
2024-03-04 20:59:09 -0300

    operacoes listas

---
+++ Aula8/operacoes_listas.py
@@ -0,0 +1,12 @@
+lista1 = ['a'] * 20
+print(lista1.count('a'))
+
+lista2 = [2,4,6,3,5,10,8]
+print(sum(lista2))
+print(min(lista2))
+print(max(lista2))
+
+lista2.sort()
+print(lista2)
+lista2.reverse()
+print(lista2)
