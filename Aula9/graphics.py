2c6e82c53a023788a18050def6bfea38f6f303b7
William Novak <novakwilliamr@gmail.com>
2024-03-06 20:52:32 -0300

    exemplo 2 e 3 graficos e linhas

--- Aula9/graphics.py
+++ Aula9/graphics.py
@@ -1,4 +1,21 @@
 import matplotlib.pyplot as plt
 
+# Exemplo 1 - Grafico
 plt.plot([1,2,3,4],[2,4,9,16]) # Define coordenadas X e Y
-plt.show() # Gera o grafico
+# plt.show() # Gera o grafico
+
+# Exemplo 2 - Grafico com listas e repeticao
+listaX = [x for x in range(10)]
+listaY = [x**2 for x in range(10)]
+
+plt.plot(listaX, listaY) # Define coordenadas conforme listas
+# plt.show()
+
+plt.plot(listaX, listaY, 'ro') # Estilo para linha (r = cor, o = formato) - Padrao b-
+# plt.show()
+
+# Exemplo 3 - Multiplas linhas
+listaY2 = [x**3 for x in range(10)]
+plt.plot(listaX, listaY,'r-')
+plt.plot(listaX, listaY2,'bo')
+# plt.show()
