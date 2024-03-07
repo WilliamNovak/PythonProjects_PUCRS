ec1f4be31b7a826e9e5e69e7380030dccd0fbb0d
William Novak <novakwilliamr@gmail.com>
2024-03-07 20:28:01 -0300

    frequencia caracteres

--- Aula9/frequencia.py
+++ Aula9/frequencia.py
@@ -1,4 +1,7 @@
-frase = input('Digite uma frase: ')
+# Contar frequencia de letras em frase e quais foram usadas ou nao
+import string
+
+frase = input('Digite uma frase: ').lower()
 dic = {}
 
 for letra in frase:
@@ -11,3 +14,10 @@ for letra in frase:
 print('Frequência de caracteres na frase:')
 for l,v in dic.items():
     print(f'{l} => {v}')
+
+todasLetras = set(string.ascii_lowercase)
+usadas      = set(dic.keys())
+naoUsadas   = set(todasLetras - usadas)
+
+print(f'Letras usadas    : {sorted(usadas)}')
+print(f'Letras não usadas: {sorted(naoUsadas)}')
