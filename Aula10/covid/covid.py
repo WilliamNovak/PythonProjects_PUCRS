a3596709152fe1f158699c09d36f4809e4e9830a
William Novak <novakwilliamr@gmail.com>
2024-03-10 17:02:04 -0300

    estatisticas covid parte 2

--- Aula10/covid/covid.py
+++ Aula10/covid/covid.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def carregaDados(arquivo):
     arq = open(arquivo, 'r', encoding='utf8')
     dados = []
@@ -66,6 +68,43 @@ def mediaNovosObitos(dados):
         total += linha[5]
     return (total / len(dados))
 
+# Percorre todas as posicoes ordenando do maior para o menor valor (Bubble Sort)
+def ordenaIncidencias(cidades, incidencias):
+    for i in range(0, len(incidencias) - 1):
+        # A cada loop desconta i, pois as ultimas posicoes ja irao estar ordenadas
+        for j in range(0, len(incidencias) - 1 - i):
+            if incidencias[j] < incidencias[j + 1]:
+                aux = incidencias[j]
+                incidencias[j] = incidencias[j + 1]
+                incidencias[j + 1] = aux
+                aux = cidades[j]
+                cidades[j] = cidades[j + 1]
+                cidades[j + 1] = aux
+
+def geraGraficoIncidencias(cidades, incidencias, quantidade):
+    cidadesQtd = []
+    incidenciasQtd = []
+    cont = 0
+    while cont < quantidade:
+        cidadesQtd.append(cidades[cont])
+        incidenciasQtd.append(incidencias[cont])
+        cont += 1
+    
+    plt.title('Incidência de COVID por 100 mil hab no RS')
+    plt.xlabel('Cidades')
+    plt.ylabel('Incidência')
+    plt.plot(cidadesQtd, incidenciasQtd)
+    plt.show()
+
+def trataIncidencias(dados, quantidade):
+    cidades = []
+    incidencias = []
+    for linha in dados:
+        cidades.append(linha[0])
+        incidencias.append(linha[3])
+    ordenaIncidencias(cidades, incidencias)
+    geraGraficoIncidencias(cidades, incidencias, quantidade)
+
 dados = carregaDados('SESRS - Coronavírus_v1.csv')
 cabecalho = dados[0].split(',')
 dadosConvertidos = converteDados(dados)
@@ -75,3 +114,5 @@ print(f'Total de óbitos no estado (atuais e novos): {obitosEstado(dadosConverti
 print(f'Cidade com mais novos casos: {cidadeMaisNovosCasos(dadosConvertidos)}')
 print(f'Cidade com menor mortalidade do estado: {cidadeMenorMortalidade(dadosConvertidos)}')
 print(f'Média de novos óbitos no estado: {mediaNovosObitos(dadosConvertidos):.2f}')
+
+trataIncidencias(dadosConvertidos, 5)
